c2a6fbdca0601cb1f4c3680b714ee845abd227e4
fallomo <37293554+fallomo@users.noreply.github.com>
2019-07-18 21:46:23 +0800

    0718
    几乎重构了主页面和搜索页面的所有逻辑，现在更接近滴滴打车了
    1.通过移动地图选取初始点（屏幕中心即为初始点）
    2.通过点击地图选取终止点
    3.点击起始/终止框进入搜索页面，输入关键词得到推荐列表，点击推荐列表自动调转到对应位置
    
    1.移除了搜索按钮，现在搜索栏正确的绑定在输入栏上了并能正确的进行跨页面数值传递了
    2.现在有了重构标识，解决了主页面多余重构导致的标识位置异常
    3.采用可触式而不是文本输入框进行起止点的选择
    4.在未输入关键词时，搜索页面显示空白而不是正在加载

--- rnlearing/component/Home.js
+++ rnlearing/component/Home.js
@@ -1,5 +1,5 @@
 import  React,{Component} from 'react'
-import {Text,View,Image,TextInput,StyleSheet,Button ,FlatList} from 'react-native';
+import {Text,View,Image,TextInput,StyleSheet,Button ,TouchableOpacity,FlatList} from 'react-native';
 import { MapView } from 'react-native-amap3d'
 import PropTypes from "prop-types";
 import NowAndToGo from './NowAndTogo'
@@ -34,32 +34,38 @@ export default class HomeScreen extends React.Component {
         latitude: '',
         Nowlongitude: '',
         Nowlatitude: '',
+        Togolongitude:'',
+        Togolatitude:'',
         Pos3:'',
         Pos4:'',
         city:'',
-        test:'',
+        test1:'null',
+        test2:'null',
         logs: [],
-        NowLocation:'',
-        Togo:'',
+        NowLocation:'当前位置',
+        mode:'',
+        Togo:'我想去',
         Searchlocation:'',
         findpath:false,
-        zoom:18
+        zoom:18,
+        monted:false,
+        searched:false,
     }
 }
 
 getRad(d){
-  return d*PI/180.0;
+  return d*3.1415926/180.0;
 }
 //暂时使用的数学计算方法，在完善车辆统计后将替换为高德api的附近派单自带计算距离
  getGreatCircleDistance(lat1,lng1,lat2,lng2){
-  var radLat1 = getRad(lat1)
-  var radLat2 = getRad(lat2);
+  var radLat1 = this.getRad(lat1)
+  var radLat2 = this.getRad(lat2);
   
   var a = radLat1 - radLat2;
-  var b = getRad(lng1) - getRad(lng2);
+  var b = this.getRad(lng1) - this.getRad(lng2);
   
   var s = 2*Math.asin(Math.sqrt(Math.pow(Math.sin(a/2),2) + Math.cos(radLat1)*Math.cos(radLat2)*Math.pow(Math.sin(b/2),2)));
-  s = s*EARTH_RADIUS;
+  s = s*6378137.0;
   s = Math.round(s*10000)/10000.0;
           
   return s;
@@ -108,7 +114,11 @@ _GetSearchValue(val) //同页面子传父用函数
 }
 
   componentDidMount() {
-  this.Getcity();
+    if(!this.state.monted)
+    {
+        this.Getcity();
+        this.setState({monted:true})
+    }
 
     // setTimeout(() => {
     //   navigator.geolocation.getCurrentPosition(
@@ -170,10 +180,10 @@ _logPressEvent = ({ nativeEvent }) => {
   this._log('onPress', nativeEvent)
   const longitude = nativeEvent.longitude;
   const latitude  =nativeEvent.latitude;
- if(nativeEvent.longitude!=this.state.Nowlongitude&&nativeEvent.latitude!=this.state.Nowlatitude){
+ if(nativeEvent.longitude!=this.state.Togolongitude&&nativeEvent.latitude!=this.state.Togolatitude){
   this.setState({
-    Nowlongitude:nativeEvent.longitude,
-    Nowlatitude:nativeEvent.latitude,
+    Togolongitude:nativeEvent.longitude,
+    Togolatitude:nativeEvent.latitude,
 },()=>{ 
 fetch("https://restapi.amap.com/v3/geocode/regeo?key=4df0ef52b83b532834ffa118afa77de5&location="+longitude+","+latitude+ "&poitype=城市&radius=1000&extensions=all&batch=false&roadlevel=0")
 .then(response=>response.json())
@@ -198,16 +208,18 @@ _logStatusChangeCompleteEvent = ({ nativeEvent }) =>
   NowLocationChange=({nativeEvent})=>{
     const longitude = nativeEvent.longitude;
     const latitude  =nativeEvent.latitude;
-   if(nativeEvent.longitude!=this.state.longitude&&nativeEvent.latitude!=this.state.latitude){
+    if(this.state.searched==true) this.setState({searched:false})
+   else if(nativeEvent.longitude!=this.state.longitude&&nativeEvent.latitude!=this.state.latitude){
     this.setState({
-      longitude:nativeEvent.longitude,
-      latitude:nativeEvent.latitude,
+      Nowlongitude:nativeEvent.longitude,
+      Nowlatitude:nativeEvent.latitude,
  },()=>{ 
   fetch("https://restapi.amap.com/v3/geocode/regeo?key=4df0ef52b83b532834ffa118afa77de5&location="+longitude+","+latitude+ "&poitype=城市&radius=1000&extensions=all&batch=false&roadlevel=0")
  .then(response=>response.json())
  .then(json=>{
    this.setState({
            city:json.regeocode.formatted_address,
+           NowLocation:json.regeocode.formatted_address
          })  
    }).catch((error)=>{
     console.log('request failed', error)
@@ -223,30 +235,56 @@ _renderItem = ({ item }) =>
   {
     this.props.navigation.navigate('Search');
   }
+
   componentWillMount()
   {
     const { params } = this.props.navigation.state;
     const Searchlocation = params ? params.Searchlocation : null;
+    const mode=params ? params.Mode:null;
     if(Searchlocation)
     {
+      const arr =Searchlocation.split(',')
       this.setState({
       Searchlocation:Searchlocation,
+      longitude:arr[0],
+      latitude:arr[1],
+      monted:true,
+      mode:mode,
+      searched:true,
        },()=>{ 
+        //  this.setState({test1:'oh'})
         fetch("https://restapi.amap.com/v3/geocode/regeo?key=4df0ef52b83b532834ffa118afa77de5&location="+Searchlocation+ "&poitype=城市&radius=1000&extensions=all&batch=false&roadlevel=0")
        .then(response=>response.json())
        .then(json=>{
-         this.setState({
-                 Togo:json.regeocode.formatted_address,
+        // this.setState({test1:'ohhh'})
+        if(mode=='Now')
+         {
+          this.setState({
+            NowLocation:json.regeocode.formatted_address,
+            Nowlongitude:arr[0],
+            Nowlatitude:arr[1],
                })  
-         }).catch((error)=>{
+          }
+          else if(mode=='To')
+          {
+           this.setState({
+             Togo:json.regeocode.formatted_address,
+             Togolongitude:arr[0],
+             Togolatitude:arr[1],
+           })  
+          }
+
+      }
+         ).catch((error)=>{
           console.log('request failed', error)
       })
-        })
+        });
+
       }
       if(this.state.NowLocation&&this.state.Togo)
       {
         this.setState({findpath:true})
-        var distance=this.getGreatCircleDistance(this.state.latitude,this.state.longitude,this.state.Nowlatitude,this.state.Nowlongitude)
+        var distance=this.getGreatCircleDistance(this.state.Nowlatitude,this.state.Nowlongitude,this.state.Togolatitude,this.state.Togolongitude)
         this.setState({test:distance})
         if(0<=distance<=100) this.setState({zoom:18})
         if(100<=distance<=1000) this.setState({zoom:17})
@@ -255,16 +293,34 @@ _renderItem = ({ item }) =>
         else this.setState({zoom:10})
       }
   }
+
+Postdata(e)
+{
+  if(e=='Now')
+  {
+    this.props.navigation.navigate('Search',{Mode:'Now',Data:this.state.NowLocation})
+  }
+  if(e=='To')
+  {
+    this.props.navigation.navigate('Search',{Mode:'To',Data:this.state.Togo})
+  }
+}
+
     render() {
       const Pos ={
         Mainpos:{
         latitude: this.state.latitude*1,
         longitude: this.state.longitude*1
       },
-      secpos:{
-        latitude: this.state.Nowlatitude*1,
-        longitude: this.state.Nowlongitude*1
-      }
+        Nowpos:{
+          latitude: this.state.Nowlatitude*1,
+          longitude: this.state.Nowlongitude*1
+        },
+        Togopos:{
+          latitude: this.state.Togolatitude*1,
+          longitude: this.state.Togolongitude*1
+        },
+
     }
 
       //测试navigatin传值用
@@ -289,28 +345,28 @@ _renderItem = ({ item }) =>
         // </View>
         <View style={styles.container}>
           <MapView
-        coordinate={Pos.Mainpos}
-        zoomLevel={this.state.zoom}
-        locationEnabled
-        locationInterval={10000}
-        distanceFilter={10}
-        onPress={this._logPressEvent}
-        onLongPress={this._logLongPressEvent}
-        onLocation={this._logLocationEvent}
-        onStatusChangeComplete={this.NowLocationChange}
-        style={styles.top}
-        >
+          coordinate={Pos.Mainpos}
+          zoomLevel={this.state.zoom}
+          locationEnabled
+          locationInterval={10000}
+          distanceFilter={10}
+          onPress={this._logPressEvent}
+          onLongPress={this._logLongPressEvent}
+          onLocation={this._logLocationEvent}
+          onStatusChangeComplete={this.NowLocationChange}
+          style={styles.top}
+          >
             <MapView.Marker
               draggable
               title='我要去'
               onDragEnd={this._onDragEvent}
-               onInfoWindowPress={this._onInfoWindowPress}
-              coordinate={Pos.secpos}
+              onInfoWindowPress={this._onInfoWindowPress}
+              coordinate={Pos.Togopos}
             />
-            <MapView.Marker image="car_icon" coordinate={Pos.Mainpos}>
+            <MapView.Marker image="flag" coordinate={Pos.Nowpos}>
               <View style={styles.defaultbox}>
               {/* <Image  source={require('../images/car_icon.png')} resizeMode={'contain'} /> */}
-                <Text>最近的出租车</Text>
+                <Text>我的位置</Text>
               </View>
             </MapView.Marker>
           </MapView>
@@ -321,17 +377,17 @@ _renderItem = ({ item }) =>
           data={this.state.logs}
           renderItem={this._renderItem}
         /> */}
-        <Text style={styles.input}>当前位置:{this.state.city}</Text>
-        {/* <Text style={styles.input}>传值测试:{this.state.Searchlocation}</Text> */}
-        <Text style={styles.input}>计算距离:{this.state.test}</Text>
-        <View style={{flex:1 ,justifyContent:'center'}}>
-                <TextInput style={styles.input} onChangeText={(NowLocation) => { this.setState({NowLocation}) }} value={this.state.NowLocation} placeholder={'我的位置'}></TextInput>
-                <TextInput style={styles.input} onChangeText={(Togo) => { this.setState({Togo}) }} value={this.state.Togo} placeholder={'我想去'}></TextInput>
+        {/* <Text style={styles.input}>测试{this.state.mode}+{this.state.test1}+{this.state.test2}+{this.state.Searchlocation},{this.state.Nowlatitude}</Text> */}
+        {/* <Text style={styles.input}>传值测试:{this.state.NowLocation}</Text> */}
+        {/* <Text style={styles.input}>计算距离:{this.state.test}</Text> */}
+        <TouchableOpacity style={{flex:1 ,justifyContent:'center'}}>
+                <Text style={styles.input} onPress={(event) => this.Postdata('Now')} key='Now'>{this.state.NowLocation}</Text>
+                <Text style={styles.input} onPress={(event) => this.Postdata('To')} key='To'>{this.state.Togo}</Text>
                 <Text style={styles.login} onPress={ () => {this.Move()} }>Move</Text>
-            </View>
+        </TouchableOpacity>
         </View>
         <View style={styles.bottom}>
-        <Button style={{flex: 1, alignItems: 'flex-end', justifyContent: 'space-between'}} color='purple' onPress={() => this.props.navigation.navigate('Search')} title="搜索功能预览"/>
+        {/* <Button style={{flex: 1, alignItems: 'flex-end', justifyContent: 'space-between'}} color='purple' onPress={() => this.props.navigation.navigate('Search')} title="搜索功能预览"/> */}
          <Button style={{flex: 1, alignItems: 'flex-end', justifyContent: 'space-between'}} onPress={() => this.props.navigation.navigate('Mine')} title="我的课程"/>
         </View>
       </View>
--- rnlearing/component/Search.js
+++ rnlearing/component/Search.js
@@ -12,14 +12,16 @@ export default class Search extends React.Component {
           datastr:'',
           data:
      [{"id":[],"name":"肯德基","district":[],"adcode":[],"location":[],"address":[],"typecode":[],"city":[]},{"id":"B0FFKEPXS2","name":"肯德基(望京西店)","district":"北京市朝阳区","adcode":"110105","location":"116.474027,39.997731","address":"望京西园4区410号综合楼1层","typecode":"050301","city":[]},{"id":"B000A7BM4H","name":"肯德基(花家地店)","district":"北京市朝阳区","adcode":"110105","location":"116.469209,39.985562","address":"花家地小区1号商业楼","typecode":"050301","city":[]},{"id":"B000A7FVJQ","name":"肯德基(中福百货店)","district":"北京市朝阳区","adcode":"110105","location":"116.463373,40.000423","address":"望京南湖东园201号楼1层","typecode":"050301","city":[]},{"id":"B000A875MK","name":"肯德基(来广营店)","district":"北京市朝阳区","adcode":"110105","location":"116.464422,40.016510","address":"香宾路66-1号","typecode":"050301","city":[]},{"id":"B000A80GPM","name":"肯德基(酒仙桥二店)","district":"北京市朝阳区","adcode":"110105","location":"116.495399,39.961907","address":"酒仙桥路39号久隆百货B1层","typecode":"050301","city":[]},{"id":"B000A9P8KT","name":"肯德基(太阳宫店)","district":"北京市朝阳区","adcode":"110105","location":"116.448473,39.971184","address":"太阳宫中路12号凯德MallF1层01-13A-14-15B","typecode":"050301","city":[]},{"id":"B000A80HAN","name":"肯德基(霄云路店)","district":"北京市朝阳区","adcode":"110105","location":"116.464837,39.959331","address":"霄云路27号中国庆安大厦1层","typecode":"050301","city":[]},{"id":"B0FFF3DEDV","name":"肯德基(凤凰汇购物中心)","district":"北京市朝阳区","adcode":"110105","location":"116.456296,39.962578","address":"曙光西里甲5号院24号楼凤凰汇购物中心B1层104号","typecode":"050301","city":[]},{"id":"B000A8ZIKF","name":"肯德基(西坝河店)","district":"北京市朝阳区","adcode":"110105","location":"116.436279,39.968924","address":"西坝河西里","typecode":"050301","city":[]}],
-          load:false
+          load:false,
+          mode:'',
+          search:''
           
         }
     }
     // [{"id":[],"name":"肯德基","district":[],"adcode":[],"location":[],"address":[],"typecode":[],"city":[]},{"id":"B0FFKEPXS2","name":"肯德基(望京西店)","district":"北京市朝阳区","adcode":"110105","location":"116.474027,39.997731","address":"望京西园4区410号综合楼1层","typecode":"050301","city":[]},{"id":"B000A7BM4H","name":"肯德基(花家地店)","district":"北京市朝阳区","adcode":"110105","location":"116.469209,39.985562","address":"花家地小区1号商业楼","typecode":"050301","city":[]},{"id":"B000A7FVJQ","name":"肯德基(中福百货店)","district":"北京市朝阳区","adcode":"110105","location":"116.463373,40.000423","address":"望京南湖东园201号楼1层","typecode":"050301","city":[]},{"id":"B000A875MK","name":"肯德基(来广营店)","district":"北京市朝阳区","adcode":"110105","location":"116.464422,40.016510","address":"香宾路66-1号","typecode":"050301","city":[]},{"id":"B000A80GPM","name":"肯德基(酒仙桥二店)","district":"北京市朝阳区","adcode":"110105","location":"116.495399,39.961907","address":"酒仙桥路39号久隆百货B1层","typecode":"050301","city":[]},{"id":"B000A9P8KT","name":"肯德基(太阳宫店)","district":"北京市朝阳区","adcode":"110105","location":"116.448473,39.971184","address":"太阳宫中路12号凯德MallF1层01-13A-14-15B","typecode":"050301","city":[]},{"id":"B000A80HAN","name":"肯德基(霄云路店)","district":"北京市朝阳区","adcode":"110105","location":"116.464837,39.959331","address":"霄云路27号中国庆安大厦1层","typecode":"050301","city":[]},{"id":"B0FFF3DEDV","name":"肯德基(凤凰汇购物中心)","district":"北京市朝阳区","adcode":"110105","location":"116.456296,39.962578","address":"曙光西里甲5号院24号楼凤凰汇购物中心B1层104号","typecode":"050301","city":[]},{"id":"B000A8ZIKF","name":"肯德基(西坝河店)","district":"北京市朝阳区","adcode":"110105","location":"116.436279,39.968924","address":"西坝河西里","typecode":"050301","city":[]}]
-    GetData()
+    GetData(data)
     {
-        fetch("https://restapi.amap.com/v3/assistant/inputtips?key=4df0ef52b83b532834ffa118afa77de5&keywords=肯德基&type=050301&location=116,39&city=广东&datatype=all")
+        fetch("https://restapi.amap.com/v3/assistant/inputtips?key=4df0ef52b83b532834ffa118afa77de5&keywords="+data+"&type=050000&location=116,39&city=广东&datatype=all")
         .then(response=>response.json())
         .then(json=>{
           this.setState({
@@ -32,7 +34,10 @@ export default class Search extends React.Component {
     }
     componentWillMount()
     {
-        this.GetData()
+      const { params } = this.props.navigation.state;
+      const data = params ? params.Data : null;
+      const mode = params ? params.Mode:null;
+      this.setState({Mode:mode})
     }
 
     // <Text>数据：{this.state.datastr}</Text>
@@ -40,15 +45,28 @@ export default class Search extends React.Component {
     
     renderLoadingView() {
       return (
-        <View style={{flex:1,justifyContent:'flex-start',alignItems:'center'}}>
+        <View style={styles.container}>
+        <TextInput style={styles.input} onChangeText={(search) => { this.setState({search:search},this.GetData(search))  }} value={this.state.search} placeholder={'请输入搜索内容'}></TextInput>
+        <Text>调试用:{this.state.Mode}</Text>
           <Text style={styles.title}>
             请稍后，正在搜索数据……
           </Text>
         </View>
       );
     }
+    renderBlank()
+    {return(
+      <View style={styles.container}>
+      <TextInput style={styles.input} onChangeText={(search) => { this.setState({search:search},this.GetData(search))  }} value={this.state.search} placeholder={'请输入搜索内容'}></TextInput>
+      <Text>调试用:{this.state.Mode}</Text>
+        <Text style={styles.title}>
+        </Text>
+      </View>
+    )
+    }
 
     render(){
+      if(this.state.search=="") return this.renderBlank()
       if(this.state.load)
       {
         return(
@@ -56,10 +74,12 @@ export default class Search extends React.Component {
             //   <Mylist data={this.state.data}/>
             // </View>
             <ScrollView style={styles.container}>
+               <TextInput style={styles.input} onChangeText={(search) => { this.setState({search:search},this.GetData(this.state.search))  }} value={this.state.search} placeholder={'请输入搜索内容'}></TextInput>
+               <Text>调试用:{this.state.Mode}</Text>
             {
               this.state.data.map((item) => {
                 return (
-                  <TouchableOpacity style={styles.item} onPress={() => { this.props.navigation.navigate('Home', {Searchlocation: item.location}) }} >
+                  <TouchableOpacity style={styles.item} onPress={() => { this.props.navigation.navigate('Home', {Mode:this.state.Mode,Searchlocation:item.location}) }} >
                     <Mylist data={item} />
                   </TouchableOpacity>
                 )
@@ -67,6 +87,7 @@ export default class Search extends React.Component {
             }
           </ScrollView>
         )
+
       }else return this.renderLoadingView() 
     }
 };
@@ -84,5 +105,14 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     color: 'black'
+  },
+  input: {
+    fontSize: 20,
+    width: 500,
+    margin: 10,
+    borderBottomWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#841584',
+    padding: 5
   },
   });
