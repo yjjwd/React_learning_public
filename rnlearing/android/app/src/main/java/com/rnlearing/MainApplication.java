a8c67d0ee4c137e6c7ab051b34a5d94e3854cff7
fallomo <37293554+fallomo@users.noreply.github.com>
2019-09-21 14:11:25 +0800

    0920
    调整版本

--- rnlearing/android/app/src/main/java/com/rnlearing/MainApplication.java
+++ rnlearing/android/app/src/main/java/com/rnlearing/MainApplication.java
@@ -3,6 +3,7 @@ package com.rnlearing;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import cn.qiuxiang.react.amap3d.AMap3DPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
             new AMap3DPackage()
       );
     }
